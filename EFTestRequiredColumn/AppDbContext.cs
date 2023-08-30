30758afeaef85faa5584a92511b67ec8eb26148f
sa-es-ir <s.esmaeelinejad@gmail.com>
2023-08-30 22:03:03 +0700

    comment codes in appdbcontext to show the issue

--- EFTestRequiredColumn/AppDbContext.cs
+++ EFTestRequiredColumn/AppDbContext.cs
@@ -10,5 +10,18 @@ public class AppDbContext : DbContext
             .UseSqlServer("connection-string");
     }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // to show the issue with these line should be commented and after you can uncomment and run the migration again
+        //modelBuilder.Entity<Student>()
+        //    .HasKey(x => x.Id);
+
+        //modelBuilder.Entity<Student>()
+        //    .HasIndex(x => x.UniqueCode)
+        //    .IsUnique();
+    }
+
     public DbSet<Customer> Customers { get; set; }
+
+    public DbSet<Student> Students { get; set; }
 }
